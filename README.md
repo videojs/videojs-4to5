fe728a6cb86636d05f4208c8e595b904fcb65e21
Pat O'Neill <pgoneill@gmail.com>
2015-08-13 17:39:15 -0400

    Support pipe-able dry run

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ This is a small shell script to help with bringing video.js 4.x code up to speed
 Run the script in a shell, pass it directories or files:
 
 ```bash
-./videojs-4to5 my-project some-file.js
+./videojs-4to5.sh my-project some-file.js
 ```
 
 All JavaScript and CSS files will be edited in place to reflect changes for video.js 5.0.
+
+### Options
+
+#### Dry Run, `-d`
+
+This won't change anything, but will print the files to be edited to STDOUT, so they can be piped, etc.
--- videojs-4to5.sh
+++ videojs-4to5.sh
@@ -1,27 +1,49 @@
 #!/bin/sh
 
+# Takes a single JavaScript filename and processes it.
+process_js(){
+    if [ $DRY = true ]; then
+        echo "$1"
+    else
+        echo "TODO: process js file $1"
+    fi
+}
+
+# Takes a single CSS filename and processes it.
+process_css(){
+    if [ $DRY = true ]; then
+        echo "$1"
+    else
+        echo "TODO: process css file $1"
+    fi
+}
+
 # Takes a single filename argument and processes it depending on its type.
 process_file(){
-    if [ -f "$1" ]
-    then
+    if [ -f "$1" ]; then
         FNAME=$(basename "$1")
         EXT="${FNAME##*.}"
-        if [ $EXT == "js" ]
-        then
-            echo "js:     $1"
-        elif [ $EXT == "css" ]
-        then
-            echo "css:    $1"
+        if [ "$EXT" == "js" ]; then
+            process_js "$1"
+        elif [ "$EXT" == "css" ]; then
+            process_css "$1"
         fi
     fi
 }
 
+# Parse options.
+DRY="false"
+
+while getopts "d" FLAG; do
+    case "${FLAG}" in
+        d) DRY="true" ;;
+    esac
+done
+
 # Loop over arguments and find all JS and CSS files not in node_modules
 # and process each of them individually.
-for X in "$@"
-do
-    if [ -d "$X" ]
-    then
+for X in "$@"; do
+    if [ -d "$X" ]; then
         for FILE in $(find "$X" -not -path "*node_modules*" -name "*.js" -or -not -path "*node_modules*" -name "*.css"); do
             process_file "$FILE"
         done
