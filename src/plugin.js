7d26f190c85b546af239f233d67f438c009c74a5
Pat O'Neill <pgoneill@gmail.com>
2015-11-16 17:06:05 -0500

    Cleanup and tests
    This unifies coding style to always use `forEach` for looping and
    updates some code because we can assume that ES5 works - video.js
    5 will fail without being in an ES5 environment!

--- src/plugin.js
+++ src/plugin.js
@@ -1,63 +1,53 @@
 (function(window, videojs) {
-  var
-    Component = videojs.getComponent('Component'),
-    keys = Object.keys(videojs.browser),
-    components = Object.keys(Component.components_),
-    oldOptions, i, key, component,
+  var Component = videojs.getComponent('Component');
 
-    // Some classes changed between video.js 4 and 5. This back-fills those
-    // to restore the old classes for styling or scripting purposes.
-    backfillClasses = {
-      'vjs-time-controls': [
-        'TimeDivider',
-        'RemainingTimeDisplay',
-        'DurationDisplay',
-        'CurrentTimeDisplay'
-      ],
-      'vjs-live-controls': [
-        'LiveDisplay'
-      ]
-    };
-
-  window.vjs = videojs;
-
-  videojs.JSON = JSON;
-  videojs.USER_AGENT = window.navigator.userAgent;
-  videojs.EventEmitter = videojs.EventTarget;
-
-  videojs.util = {
-    mergeOptions: function() {
-      videojs.log.warn('videojs.util.mergeOptions is deprecated. ' +
-        'Use videojs.mergeOptions instead.');
-      return videojs.mergeOptions.apply(null, arguments);
-    }
+  // Some classes changed between video.js 4 and 5. This back-fills those
+  // to restore the old classes for styling or scripting purposes.
+  var backfillClasses = {
+    'vjs-time-controls': [
+      'TimeDivider',
+      'RemainingTimeDisplay',
+      'DurationDisplay',
+      'CurrentTimeDisplay'
+    ],
+    'vjs-live-controls': [
+      'LiveDisplay'
+    ]
   };
 
-  for (i = 0; i < keys.length; i++) {
-    key = keys[i];
+  // Map properties of `videojs.browser` onto `videojs`.
+  Object.keys(videojs.browser).forEach(function(key) {
     videojs[key] = videojs.browser[key];
-  }
+  });
 
-  Component.oldExtend_ = Component.extend;
+  // Replace the static `extend` method of components with one that works
+  // with the assumptions of video.js 4.x, while retaining the new behavior
+  // in `extend5_`.
+  Component.extend5_ = Component.extend;
   Component.extend = function(proto) {
     if (proto.remainingTime && !proto.scrubbing) {
       proto.scrubbing = function() {};
     }
-    return this.oldExtend_(proto);
+    return this.extend5_(proto);
   };
 
-  for (i = 0; i < components.length; i++) {
-    component = components[i];
-    videojs[component] = videojs.getComponent(component);
-    if (!videojs[component].extend) {
-      videojs[component].oldExtend_ = Component.oldExtend_;
-      videojs[component].extend = Component.extend;
-    }
-  }
+  Object.keys(Component.components_).forEach(function(compName) {
 
+    // Map each component onto `videojs` itself.
+    videojs[compName] = videojs.getComponent(compName);
+
+    // If the component does not have a static `extend` method, copy over
+    // the `Component.extend` static method handling.
+    if (!videojs[compName].extend) {
+      videojs[compName].extend5_ = Component.extend5_;
+      videojs[compName].extend = Component.extend;
+    }
+  });
+
+  // Backfill any changed HTML classes onto the relevant components.
   Object.keys(backfillClasses).forEach(function(className) {
-    backfillClasses[className].forEach(function(name) {
-      var Component = videojs.getComponent(name);
+    backfillClasses[className].forEach(function(compName) {
+      var Component = videojs.getComponent(compName);
       var createEl = Component.prototype.createEl;
 
       Component.prototype.createEl = function() {
@@ -68,22 +58,41 @@
     });
   });
 
-  oldOptions = videojs.Player.prototype.options;
-  videojs.Player.prototype.options = function() {
-    var options = oldOptions.call(this);
-    if (Object.prototype.toString.call(options.children) === '[object Array]') {
-      for (var i = 0; i < options.children.length; i++) {
-        var childName = options.children[i];
+  // The `children` property of `options_` used to be an object, but it is
+  // now an array. This restores the old behavior of that property (while
+  // retaining array behavior).
+  Component.prototype.options5_ = Component.prototype.options;
+  Component.prototype.options = function() {
+    var options = Component.prototype.options5_.apply(this, arguments);
+    if (Array.isArray(options.children)) {
+      options.children.forEach(function(childName) {
         options.children[childName] = this.getChild(childName).options_;
-      }
+      });
     }
-
     return options;
   };
 
+  // Restore missing/previously deprecated objects and methods.
+  window.vjs = videojs;
+  videojs.JSON = JSON;
+  videojs.USER_AGENT = window.navigator.userAgent;
+  videojs.EventEmitter = videojs.EventTarget;
+
+  videojs.util = {
+    mergeOptions: function() {
+      videojs.log.warn([
+        'videojs.util.mergeOptions is deprecated.',
+        'Use videojs.mergeOptions instead.'
+      ].join(' '));
+      return videojs.mergeOptions.apply(videojs, arguments);
+    }
+  };
+
   videojs.round = function(x, y) {
-    videojs.log.warn('videojs.round(x, y) is deprecated. ' +
-      'Use Number(x.toFixed(y)) instead.');
+    videojs.log.warn([
+      'videojs.round(x, y) is deprecated.',
+      'Use Number(x.toFixed(y)) instead.'
+    ].join(' '));
     return Number(x.toFixed(y));
   };
 
