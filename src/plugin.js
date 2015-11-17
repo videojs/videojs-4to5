b823e84a68b02cf441cbba7bf219c23b82a1b9a1
Pat O'Neill <pgoneill@gmail.com>
2015-11-17 10:27:31 -0500

    Naming improvements, moved things around

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,13 @@
 (function(window, videojs) {
   var Component = videojs.getComponent('Component');
 
+  // Cache new video.js 5.x methods that we will override for better
+  // backward compatibility.
+  var originals = {
+    extend: Component.extend,
+    options: Component.prototype.options
+  };
+
   // Some classes changed between video.js 4 and 5. This back-fills those
   // to restore the old classes for styling or scripting purposes.
   var backfillClasses = {
@@ -15,20 +22,27 @@
     ]
   };
 
-  // Map properties of `videojs.browser` onto `videojs`.
-  Object.keys(videojs.browser).forEach(function(key) {
-    videojs[key] = videojs.browser[key];
-  });
+  // The `children` property of `options_` used to be an object, but it is
+  // now an array. This restores the old behavior of that property (while
+  // retaining array behavior).
+  Component.prototype.options = function() {
+    var options = originals.options.apply(this, arguments);
+    if (Array.isArray(options.children)) {
+      options.children.forEach(function(childName) {
+        options.children[childName] = this.getChild(childName).options_;
+      });
+    }
+    return options;
+  };
 
   // Replace the static `extend` method of components with one that works
   // with the assumptions of video.js 4.x, while retaining the new behavior
-  // in `extend5_`.
-  Component.extend5_ = Component.extend;
+  // in `originals.extend`.
   Component.extend = function(proto) {
     if (proto.remainingTime && !proto.scrubbing) {
       proto.scrubbing = function() {};
     }
-    return this.extend5_(proto);
+    return originals.extend(proto);
   };
 
   Object.keys(Component.components_).forEach(function(compName) {
@@ -37,9 +51,9 @@
     videojs[compName] = videojs.getComponent(compName);
 
     // If the component does not have a static `extend` method, copy over
-    // the `Component.extend` static method handling.
+    // the `Component.extend` static method because it is not otherwise
+    // inherited.
     if (!videojs[compName].extend) {
-      videojs[compName].extend5_ = Component.extend5_;
       videojs[compName].extend = Component.extend;
     }
   });
@@ -47,10 +61,10 @@
   // Backfill any changed HTML classes onto the relevant components.
   Object.keys(backfillClasses).forEach(function(className) {
     backfillClasses[className].forEach(function(compName) {
-      var Component = videojs.getComponent(compName);
-      var createEl = Component.prototype.createEl;
+      var SpecificComponent = videojs.getComponent(compName);
+      var createEl = SpecificComponent.prototype.createEl;
 
-      Component.prototype.createEl = function() {
+      SpecificComponent.prototype.createEl = function() {
         var el = createEl.apply(this, arguments);
         el.className += ' ' + className;
         return el;
@@ -58,19 +72,10 @@
     });
   });
 
-  // The `children` property of `options_` used to be an object, but it is
-  // now an array. This restores the old behavior of that property (while
-  // retaining array behavior).
-  Component.prototype.options5_ = Component.prototype.options;
-  Component.prototype.options = function() {
-    var options = Component.prototype.options5_.apply(this, arguments);
-    if (Array.isArray(options.children)) {
-      options.children.forEach(function(childName) {
-        options.children[childName] = this.getChild(childName).options_;
-      });
-    }
-    return options;
-  };
+  // Map properties of `videojs.browser` onto `videojs`.
+  Object.keys(videojs.browser).forEach(function(key) {
+    videojs[key] = videojs.browser[key];
+  });
 
   // Restore missing/previously deprecated objects and methods.
   window.vjs = videojs;
